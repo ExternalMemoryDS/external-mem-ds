25867d39718b085e66e7f30f95419d04c6b1ddf7
ajinkya-k <ajinkyakokandakar@gmail.com>
2015-12-31 18:44:41 +0530

    Added findInNode for InternalNode
    Signed-off-by: ajinkya-k <ajinkyakokandakar@gmail.com>

--- src/btree.h
+++ src/btree.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cstring>
 
+using namespace	std;
 
 //Type Definitions
 typedef long blocknum_t;
@@ -115,7 +116,7 @@ private:
 	std::vector<blocknum_t> child_block_numbers;
 
 public:
-	InternalNode(blocknum_t block_number, long M, bool _isRoot = false) {
+	InternalNode(blocknum_t block_number, long M, bool _isRoot = false): BTreeNode(block_number, M) {
 		child_block_numbers.reserve(child_block_numbers.size() + (M + 1));
 	};
 
@@ -279,3 +280,20 @@ BTreeNode<K, V>* BTree<K, V>::getNodeFromBlockNum(blocknum_t block_number) {
 
 	return new_node;
 };
+
+template <typename K, typename V>
+blocknum_t InternalNode<K,V>::findInNode(const K& find_key){
+	int i;
+	// while((i <= M) && (find_key <= child_block_numbers[i])){
+	// 	i = i+1;
+	// }
+	typename std::list<K>::const_iterator key_iter = (this->keys).begin();
+	typename std::list<blocknum_t>::const_iterator block_iter = this->child_block_numbers.begin();
+
+	while((key_iter != (this->keys).end()) && (find_key <= *key_iter)){
+		++key_iter;
+		++block_iter;
+	}
+	--block_iter;
+	return *block_iter;
+}

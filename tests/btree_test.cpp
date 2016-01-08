0112e2225edc997afc3980352d1e735597e93183
Deven Bansod <devenbansod.bits@gmail.com>
2016-01-08 23:43:33 +0530

    added test file for BTREE
    Signed-off-by: Deven Bansod <devenbansod.bits@gmail.com>

---
+++ tests/btree_test.cpp
@@ -0,0 +1,84 @@
+#include "btree.h"
+#include <random>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+#define NUM_INSERT 5
+
+struct test_Comparator {
+	test_Comparator() {}
+	int operator()(int x, int y) const { return x > y; }
+
+};
+
+int main()
+{
+	BTree<int, int, test_Comparator>* btree_test = new BTree<int, int, test_Comparator>("./btree_test", 512);
+
+	std::default_random_engine generator;
+	std::uniform_int_distribution<int> distribution(1,1000);
+
+	auto dice = std::bind ( distribution, generator );
+
+	for(auto i = 1; i <= NUM_INSERT; i++)
+	btree_test->insertElem(dice(), dice());
+
+	std::cout << btree_test->size() << std::endl;
+	std::cout << std::endl;
+
+	// for (auto it = 0; it < exvec.size(); it++)
+	// {
+	// 	std::cout << exvec[it] << std::endl;
+	// }
+
+	std::cout << std::endl;
+
+	// delete btree_test;
+
+	//exvec.insert(2, 5);
+// 	std::vector<int> ins_vec;
+// 	for(auto i = 1; i<=231; i++)
+// 		ins_vec.push_back(dice());
+// 	exvec.insert(vector<int>::iterator(16,&exvec), ins_vec.begin(), ins_vec.end());
+
+// 	for (auto it = 0; it < exvec.size(); it++)
+// 	{
+// 		std::cout << exvec[it] << std::endl;
+// 	}
+
+// 	//exvec.clear();
+
+// 	std::cout << std::endl;
+// 	std::cout << exvec.size() << std::endl;
+
+// 	exvec.erase(vector<int>::iterator(2,&exvec), vector<int>::iterator(8250,&exvec));
+
+// 	std::cout << std::endl;
+
+// 	for (auto it = exvec.begin(); it != exvec.end(); it++)
+// 	{
+// 		std::cout << *it << std::endl;
+// 	}
+
+// 	std::cout << std::endl;
+
+// 	for (auto it = exvec.rbegin(); it != exvec.rend(); it++)
+// 	{
+// 		std::cout << *it << std::endl;
+// 		*it = 3;
+// 	}
+
+// 	std::cout << std::endl;
+
+// 	for (auto it = exvec.cbegin(); it != exvec.cend(); it++)
+// 	{
+// 		std::cout << *it << std::endl;
+// // 		*it = 5;
+// 	}
+
+// 	std::cout << std::endl;
+// 	std::cout << exvec.size() << std::endl;
+
+	return 0;
+}
